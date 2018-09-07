a1a3583d6b4befa5a2f0e58c1e720d9d19d7a2d9
Stefan Ugrinov <stefanugrinov@MacBook-Pro-4.local>
2018-09-07 15:44:07 +0200

    Add validation hook as an example of hooks

---
+++ hooks/validation/index.js
@@ -0,0 +1,35 @@
+
+module.exports = strapi => {
+  const hook = {
+
+    /**
+     * Default options
+     */
+
+    defaults: {
+      validation: {
+        path: '/public/validation'
+      }
+    },
+
+    /**
+     * Initialize the hook
+     */
+
+    initialize: cb => {
+    
+      // a hook example
+      // it's enabled by adding 
+      // "hook": {
+      //   "validation": {
+      //     "enabled": true
+      //   }
+      //   ...other hooks
+      // }
+
+      cb();
+    }
+  };
+
+  return hook;
+};
